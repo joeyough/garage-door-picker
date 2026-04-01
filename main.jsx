78d6e73c23715437140c99aeac9f5c7b88a919e0
joeyough <joey@calmplimentz.com>
2026-04-01 09:16:44 -0600

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GarageDoorVisualPicker from './GarageDoorVisualPicker.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GarageDoorVisualPicker />
+  </React.StrictMode>
+)
